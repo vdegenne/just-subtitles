57357ae99eb59368760158f113fbd18405b6da61
vdegenne <vdegenne@gmail.com>
2019-03-22 11:26:02 +0100

    lyrics update + new presentation

--- presentation.js
+++ presentation.js
@@ -8,7 +8,6 @@ import(`${dirpath}/meta.js`).then(_meta => {
 const getCover = async () => {
   let cover = new Image()
   cover.id = 'cover'
-  cover.style.height = '100%'
   return await new Promise(resolve => {
     cover.src = `${dirpath}/cover.jpg`
     cover.onerror = () => {
@@ -23,9 +22,29 @@ export const presentationTemplate = async subtitle => {
 
   return html`
   <style>
-    #background {
+    #content {
+      font-family: Roboto;
+      letter-spacing: 1px;
+
+      --first-color: blue;
+      --second-color: red;
+    }
+
+
+    .layer {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    /* #background {
       background: #000;
-      z-index: -1
+      z-index: -3;
     }
     #songtitle {
       position: absolute;
@@ -45,76 +64,115 @@ export const presentationTemplate = async subtitle => {
       margin: 0 0 0 6px;
     }
 
-    .kr {
-      font-family: NanumSquare;
-      font-weight: 100;
-      line-height: 44px;
-      font-size: 32px;
-    }
-    .fr {
-      color: #71b0ff;
-      font-size: 24px;
-      line-height: 34px;
-    }
+
     
     #content {
       display: flex;
       width: 100%;
       padding: 0px 40px;
       box-sizing: border-box;
-    }
-    #subtitlesContainer {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      flex: 1;
-      text-align: center;
-      min-height: 20px;
+      height: 100%;
     }
 
     .subtitle {
-      margin: 0 0 36px;
-      white-space: pre-wrap;
-      font-weight: 100;
-      max-width: 500px;
+
+
     }
     .subtitle:last-of-type {
       margin: 0;
-    }
-
-    #cover-container {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-    }
-    #cover {
-      display: block;
-      height: 375px !important;
-    }
-    #meta-name {
-      padding: 4px 2px;
-      opacity: .5;
-      font-size: 60%;
-    }
+    }*/
   </style>
 
   <div id="content">
-    <div id="cover-container">
-      ${cover}
-      <div id="meta-name">${meta.name}</div>
+    <style>
+      #background-layer {
+        width:200px;
+        height: 100%;
+        background: var(--first-color);
+      }
+    </style>
+    <div class="layer" id="background-layer"></div>
+
+    <!-- COVER -->
+    <style>
+      #cover-layer {
+        justify-content: flex-start;
+        padding: 0 0 0 40px;
+      }
+      #cover-container {
+        position: relative;
+        top: 10px;
+      }
+      #cover {
+        display: block;
+        height: 320px;
+        width: 320px;
+      }
+      #meta-name {
+        padding: 4px 2px;
+        opacity: .5;
+        font-size: 60%;
+        color: #fff;
+      }
+    </style>
+    <div class="layer" id="cover-layer">
+      ${cover
+        ? html`
+        <div id="cover-container">
+          ${cover}
+          <div id="meta-name">${meta.name}</div>
+        </div>`
+        : null}
     </div>
-    <div id="subtitlesContainer" class="draggable transparent">
-      ${subtitle.langs &&
-        subtitle.langs.map(
-          lang =>
-            subtitle[lang].trim()
-              ? html`<div class="subtitle ${lang}">${subtitle[
-                  lang
-                ].trim()}</div>`
-              : null
-        )}
+
+    <style>
+      #lyrics-layer {
+        justify-content: flex-end;
+        padding: 0 40px 0 0;
+      }
+      #subtitlesContainer {
+        width: 900px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .subtitle {
+        margin: 0 0 36px;
+        white-space: pre-wrap;
+        width: 600px;
+      }
+      .subtitle:last-of-type {
+        margin: 0;
+      }
+
+      .kr {
+        font-family: NanumSquare;
+        line-height: 44px;
+        font-size: 32px;
+        letter-spacing: 1px;
+      }
+      .fr {
+        color: var(--second-color);
+        font-size: 24px;
+        line-height: 34px;
+      }
+    </style>
+    <div class="layer" id="lyrics-layer">
+      <div id="subtitlesContainer" class="draggable transparent">
+        ${subtitle.langs &&
+          subtitle.langs.map(
+            lang =>
+              subtitle[lang].trim()
+                ? html`<div class="subtitle ${lang}">${subtitle[
+                    lang
+                  ].trim()}</div>`
+                : null
+          )}
+      </div>
     </div>
   </div>
+  ${meta.style}
   `
 }
